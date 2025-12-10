e9af99d85ae4a24c00eec447b88217d153948b2e
Mike Kay <mike@tauntdigital.co.uk>
2025-12-10 12:05:04 +0000

    testing

--- web/app/themes/blankslate/page.php
+++ web/app/themes/blankslate/page.php
@@ -5,7 +5,13 @@
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
             <header class="header">
-                <h1 class="entry-title" itemprop="name"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
+                
+                <h1 class="entry-title" itemprop="name">
+                    <?php the_title(); ?> TESTING
+                </h1> 
+                
+                <?php edit_post_link(); ?>
+                
             </header>
 
             <div class="entry-content" itemprop="mainContentOfPage">
@@ -15,7 +21,7 @@
                 <?php the_content(); ?>
                 <div class="entry-links"><?php wp_link_pages(); ?></div>
             </div>
-            
+
         </article>
 
 <?php endwhile;
